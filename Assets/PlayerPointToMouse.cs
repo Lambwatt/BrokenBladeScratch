57a9d8c19f0823f902118df5208d07b6b1a875c5
Michael Pattie <mjpattie@gmail.com>
2016-01-29 21:07:42 -0800

    Added cylinder that points to mouse.

---
+++ Assets/PlayerPointToMouse.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerPointToMouse : MonoBehaviour {
+	
+	[HideInInspector] public float m_angleToMouse = 0.0f;
+	public Camera m_camera;
+	// Update is called once per frame
+	void Update () {
+
+		Ray mouseRay = m_camera.ScreenPointToRay(Input.mousePosition);
+		Plane floor = new Plane(Vector3.up, 0.0f);
+
+		float dist;
+
+		if (floor.Raycast(mouseRay, out dist))
+		{
+			Vector3 point = mouseRay.GetPoint(dist);
+			m_angleToMouse = Mathf.Atan2(point.z-transform.position.z, point.x-transform.position.x)*Mathf.Rad2Deg;
+			Debug.Log (m_angleToMouse);
+		}
+
+
+	}
+
+
+}
---
+++ Assets/PointToMouse.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PointToMouse : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+		transform.position = transform.parent.position + new Vector3(1.0f, 0.0f, 0.0f);
+		transform.rotation = new Quaternion(0.0f, 0.0f, 0.0f, 0.0f);
+		transform.RotateAround(Vector3.forward, 90.0f*Mathf.Deg2Rad);
+		transform.RotateAround(transform.parent.position, Vector3.up, -GetComponentInParent<PlayerPointToMouse>().m_angleToMouse);
+	}
+}
