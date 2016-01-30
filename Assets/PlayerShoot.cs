6d4d40a345a7ee4e8a1c13aea2f3f8f9d023d840
Michael Pattie <mjpattie@gmail.com>
2016-01-29 23:07:48 -0800

    Added shooting toward the mouse.

--- Assets/PlayerShoot.cs
+++ Assets/PlayerShoot.cs
@@ -5,6 +5,8 @@ public class PlayerShoot : MonoBehaviour {
 
 	[HideInInspector] public float m_angleToMouse = 0.0f;
 	public Camera m_camera;
+	public Transform projectile;
+	public float shotSpeed;
 
 	// Update is called once per frame
 	void Update () {
@@ -17,8 +19,7 @@ public class PlayerShoot : MonoBehaviour {
 		if (floor.Raycast(mouseRay, out dist))
 		{
 			Vector3 point = mouseRay.GetPoint(dist);
-			m_angleToMouse = Mathf.Atan2(point.z-transform.position.z, point.x-transform.position.x)*Mathf.Rad2Deg;
-			Debug.Log (m_angleToMouse);
+			m_angleToMouse = -Mathf.Atan2(point.z-transform.position.z, point.x-transform.position.x)*Mathf.Rad2Deg;
 		}
 
 		if(Input.GetKeyDown(KeyCode.Space))
@@ -27,6 +28,21 @@ public class PlayerShoot : MonoBehaviour {
 	}
 
 	void shoot(){
+		Vector3 projectilePosition = this.transform.position;
+		projectilePosition += this.transform.right * 1.0f;
+		Transform clone = Instantiate(projectile, projectilePosition, Quaternion.identity) as Transform;
+		Debug.Log (clone);
 
+//		Projectile shot = clone.GetComponent<Projectile>();
+//		Debug.Log (shot);
+
+		clone.RotateAround(this.transform.position, Vector3.up, GetComponentInParent<PlayerShoot>().m_angleToMouse);
+		Matrix4x4 rotMatrix = new Matrix4x4();
+		rotMatrix.SetTRS(new Vector3(), Quaternion.Euler(0.0f, m_angleToMouse, 0.0f), new Vector3(1.0f, 1.0f, 1.0f));
+		clone.GetComponent<Rigidbody>().velocity = rotMatrix.MultiplyVector(Vector3.right);
+		clone.GetComponent<Rigidbody>().velocity *= shotSpeed; 
+
+
+		 
 	}
 }
--- Assets/Projectile.cs
+++ Assets/Projectile.cs
@@ -2,14 +2,12 @@
 using System.Collections;
 
 public class Projectile : MonoBehaviour {
-
-	public Vector3 direction;
-	public float speed = 0;
+	
+	//public float speed = 0;
 
 	// Update is called once per frame
 	void Update () {
-		transform.position += direction*speed*Time.deltaTime;
+		//transform.position += transform.right*speed*Time.deltaTime;
 	}
-
-
+	
 }
