950b5b0692ab891d9e01a73d1b629ae71d156323
Michael Pattie <mjpattie@gmail.com>
2016-01-29 19:12:40 -0800

    Added wasd movement.

---
+++ Assets/PlayerMove.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMove : MonoBehaviour {
+
+	public float speed;
+
+	// Update is called once per frame
+	void Update () {
+
+		Vector3 step = new Vector3(0.0f, 0.0f, 0.0f);
+
+		if(Input.GetKey(KeyCode.W))
+			step.z+=speed*Time.deltaTime;
+		if(Input.GetKey(KeyCode.S))
+			step.z-=speed*Time.deltaTime;
+		if(Input.GetKey(KeyCode.D))
+			step.x+=speed*Time.deltaTime;
+		if(Input.GetKey(KeyCode.A))
+			step.x-=speed*Time.deltaTime;
+
+		transform.position+=step;
+	}
+}
