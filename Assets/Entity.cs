217f58896a9e6301cddc05f2a906654456ecaef1
Michael Pattie <mjpattie@gmail.com>
2016-01-30 17:07:17 -0800

    Added health bars to prefabs and got them on screen. Need to handle deduction.

--- Assets/Entity.cs
+++ Assets/Entity.cs
@@ -6,9 +6,13 @@ public class Entity : MonoBehaviour {
 
 	public Marks[] marks = new Marks[MARKS.num];
 	public int hitPoints;
+	private int totalHealth;
 	public Canvas canvas;
 	Text water;
 	Text fire;
+	RectTransform healthBar;
+	RectTransform totalBar;
+	RectTransform remainingBar;
 	Camera cam;
 
 	// Use this for initialization
@@ -17,6 +21,8 @@ public class Entity : MonoBehaviour {
 
 		if(hitPoints == 0)
 			hitPoints = 100;
+
+		totalHealth = hitPoints;
 
 		for(int i = 0; i<MARKS.num; i++){
 			marks[i] = new Marks();
@@ -29,6 +35,14 @@ public class Entity : MonoBehaviour {
 		fire = Instantiate(Resources.Load<Text>("MarkText"));
 		fire.transform.SetParent(canvas.transform);
 		fire.rectTransform.anchoredPosition = cam.WorldToScreenPoint(transform.position + new Vector3(0.5f, 0.0f, 0.5f));
+
+		healthBar = Instantiate(Resources.Load<GameObject>("HealthBar")).GetComponent<RectTransform>();
+		healthBar.SetParent(canvas.transform);
+		healthBar.anchoredPosition = cam.WorldToScreenPoint(transform.position + new Vector3(0.0f, 0.0f, 0.9f));
+		//healthBar.GetComponent<CanvasGroup>().alpha = 0;
+
+		totalBar = healthBar.GetComponentsInChildren<RectTransform>()[0]; 
+		remainingBar = healthBar.GetComponentsInChildren<RectTransform>()[1]; 
 	}
 	
 	// Update is called once per frame
@@ -53,6 +67,10 @@ public class Entity : MonoBehaviour {
 			fire.text = "";
 		fire.rectTransform.anchoredPosition = cam.WorldToScreenPoint(transform.position + new Vector3(0.5f, 0.0f, 0.5f));
 
+		if(hitPoints<totalHealth){
+			healthBar.GetComponent<CanvasGroup>().alpha = 1;
+		}
+		healthBar.anchoredPosition = cam.WorldToScreenPoint(transform.position + new Vector3(0.0f, 0.0f, 0.9f));
 	}
 
 	public void dealDamage(int d){
